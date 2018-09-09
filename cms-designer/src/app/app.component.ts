cbe9bea94641c28e89ec2ee4d9a5926490e50a40
basilkot <basilkot@gmail.com>
2018-09-09 18:04:49 +0200

    add store url setting

--- cms-designer/src/app/app.component.ts
+++ cms-designer/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'cms-designer';
+    storeUrl: SafeUrl;
+
+    constructor(private sanitizer: DomSanitizer) { }
+
+    ngOnInit(): void {
+        this.storeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(environment.storeUrl);
+    }
 }
