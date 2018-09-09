132dcb810ce0310c4c1b3b8b37b936c4f63f8731
basilkot <basilkot@gmail.com>
2018-09-09 18:05:28 +0200

    store url settings

--- cms-designer/src/app/components/preview/preview.component.ts
+++ cms-designer/src/app/components/preview/preview.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SafeUrl } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-preview',
-  templateUrl: './preview.component.html',
-  styleUrls: ['./preview.component.scss']
+    selector: 'app-preview',
+    templateUrl: './preview.component.html',
+    styleUrls: ['./preview.component.scss']
 })
 export class PreviewComponent implements OnInit {
 
-  constructor() { }
+    @Input() storeUrl: SafeUrl;
 
-  ngOnInit() { }
+    constructor() { }
+
+    ngOnInit() { }
 
 }
