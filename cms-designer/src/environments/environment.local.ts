3a84c3058ce02f38211542bfa4b4e200b319c089
Basil Kotov <basilkot@gmail.com>
2018-10-11 16:42:33 +0200

    prepare app for platform context

---
+++ cms-designer/src/environments/environment.local.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  storeUrl: 'http://localhost:2082/designer-preview',
+  baseUrl: '/admin/cms-designer/',
+  platformUrl: 'http://localhost/admin',
+  contentUrl: '/api/content/cms/pages/',
+  apiKey: '4f35d297210e4bfbb827ea33089a59f4'
+};
