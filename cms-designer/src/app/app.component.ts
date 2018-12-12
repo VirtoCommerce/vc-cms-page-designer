5a92c3a8a2963fae0015dc64395ad29de2a57ac1
Basil Kotov <basilkot@gmail.com>
2018-12-12 17:14:48 +0200

    add layout to get parameters for preview

--- cms-designer/src/app/app.component.ts
+++ cms-designer/src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { SafeUrl } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 import { Store, select } from '@ngrx/store';
+import { filter } from 'rxjs/operators';
 import * as fromRoot from './state';
+import * as fromEditor from './modules/editor/state';
 
 @Component({
     selector: 'app-root',
@@ -16,10 +18,14 @@ export class AppComponent implements OnInit {
 
     previewLoading$: Observable<boolean>;
 
-    constructor(private store: Store<fromRoot.State>, private urls: ApiUrlsService) { }
+    constructor(private store: Store<fromRoot.State>, private editorStore: Store<fromEditor.State>, private urls: ApiUrlsService) { }
 
     ngOnInit(): void {
-        this.storeUrl = this.urls.getStoreUrl();
+        this.editorStore.pipe(select(fromEditor.getPage)).pipe(
+            filter(x => !!x)
+        ).subscribe(x => {
+            this.storeUrl = this.urls.getStoreUrl(<string>x.settings['layout']);
+        });
 
         this.previewLoading$ = this.store.pipe(select(fromRoot.getLoading));
     }
