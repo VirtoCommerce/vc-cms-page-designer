ee4dbb626831be53341b99b438b412deaca89d2c
Basil Kotov <basilkot@gmail.com>
2018-12-24 16:58:15 +0200

    add perf debug

--- cms-designer/src/main.ts
+++ cms-designer/src/main.ts
@@ -1,12 +1,20 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, ApplicationRef } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { enableDebugTools } from '@angular/platform-browser';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+    .then(moduleRef => {
+        if (!environment.production) {
+            const appRef = moduleRef.injector.get(ApplicationRef);
+            const componentRef = appRef.components[0];
+            enableDebugTools(componentRef);
+        }
+    })
+    .catch(err => console.log(err));
