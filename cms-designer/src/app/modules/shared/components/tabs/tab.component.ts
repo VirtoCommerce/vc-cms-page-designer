c1f9f4b6834ea3fb5377a21f10e51dcf37586b15
Basil Kotov <basilkot@gmail.com>
2019-02-12 17:09:50 +0200

    fix tabs

--- cms-designer/src/app/modules/shared/components/tabs/tab.component.ts
+++ cms-designer/src/app/modules/shared/components/tabs/tab.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'app-tab',
-    template: `<div [hidden]="!active"><ng-content></ng-content></div>`
+    templateUrl: './tab.component.html'
 })
 export class TabComponent implements OnInit {
 
