e0d4ea1005bad268c3e9d9389f80a1d201e20ee0
Basil Kotov <basilkot@gmail.com>
2019-03-15 17:04:35 +0200

    add publish config

--- store/src/environment.ts
+++ store/src/environment.ts
@@ -1,4 +1,5 @@
 export const Environment = {
     RenderBlockApiUrl: '/designer-preview/block',
-    DesignerUrl: 'http://localhost/'
+    // DesignerUrl: 'http://localhost/'
+    DesignerUrl: 'http://vc-admin-test.azurewebsites.net/designer'
 }
