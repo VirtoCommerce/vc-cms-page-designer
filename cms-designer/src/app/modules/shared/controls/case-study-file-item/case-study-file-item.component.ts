23cfa8a5959352d5b7bccbb27a5b32e2f6205bad
Sergey Podguzov <spo@virtoway.com>
2019-07-25 19:25:34 +0500

    FE: Case Study File Block has been added for CMS.

---
+++ cms-designer/src/app/modules/shared/controls/case-study-file-item/case-study-file-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { BaseControlComponent } from '../base-control.component';
+import { StringControlDescriptor } from '@shared/models';
+
+@Component({
+    selector: 'case-study-file-item',
+    templateUrl: './case-study-file-item.html',
+    styleUrls: ['./case-study-file-item.scss']
+})
+export class CaseStudyFileItemComponent extends BaseControlComponent<StringControlDescriptor> {
+
+    @ViewChild('control') control: ElementRef;
+
+    constructor() {
+        super();
+    }
+
+    getFocusableControl(): ElementRef {
+        return this.control;
+    }
+}
